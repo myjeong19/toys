54b391c6606675cee364d6d332a12208a127c68d
myjeong19@naver.com <myjeong19@naver.com>
2023-11-27 22:19:30 +0900

    Feat. Add a Card 인풋 열기 닫기

--- kanban/src/components/main/AddTodo.jsx
+++ kanban/src/components/main/AddTodo.jsx
@@ -1,12 +1,13 @@
 import classes from './css/AddTodo.module.css';
 
-export const AddTodo = () => {
+export const AddTodo = ({ onAddTodo }) => {
+  const handleOnAddTodo = () => onAddTodo(false);
   return (
     <div className={classes.form}>
       <input type='text' />
       <div className={classes['button-group']}>
         <button>Add</button>
-        <button>X</button>
+        <button onClick={handleOnAddTodo}>X</button>
       </div>
     </div>
   );
--- kanban/src/components/main/List.jsx
+++ kanban/src/components/main/List.jsx
@@ -1,11 +1,16 @@
 import { useState } from 'react';
 import classes from './css/List.module.css';
+import { AddTodo } from './AddTodo';
 
 export const List = ({ status }) => {
+  const [addTodo, setAddTodo] = useState(false);
+  const handleOnAddTodo = () => setAddTodo(!addTodo);
+
   return (
     <ul className={classes.list}>
       <h3>{status}</h3>
-      <button>Add a Card</button>
+      {!addTodo && <button onClick={handleOnAddTodo}>Add a Card</button>}
+      {addTodo && <AddTodo onAddTodo={setAddTodo} />}
     </ul>
   );
 };
