1fff57bc4f05aded93a35dc1a6eb30a2181e7d8b
myjeong19@naver.com <myjeong19@naver.com>
2023-12-17 21:11:01 +0900

    Feat. Creating and nesting components

---
+++ react-docs/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+import { Button } from './components/Button';
+
+function App() {
+  return (
+    <>
+      <h1>Welcome to my app</h1>
+      <Button></Button>
+    </>
+  );
+}
+
+export default App;
---
+++ react-docs/src/components/Button.jsx
@@ -0,0 +1,3 @@
+export function Button() {
+  return <button>I'm a button</button>;
+}
