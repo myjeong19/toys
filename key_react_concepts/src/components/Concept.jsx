c6fc9b64892529b024d76842b2bc9cedaae3ae2c
myjeong19@naver.com <myjeong19@naver.com>
2023-12-21 18:15:21 +0900

    Create tic-tac-toe

--- key_react_concepts/src/components/Concept.jsx
+++
@@ -1,9 +0,0 @@
-export const Concept = ({ title, image, description, key }) => {
-  return (
-    <li key={key} className='concept'>
-      <img src={image} alt={description} />
-      <h2>{title}</h2>
-      <p>{description}</p>
-    </li>
-  );
-};
--- react-docs/src/components/Button.jsx
+++
@@ -1,3 +0,0 @@
-export function Button() {
-  return <button>I'm a button</button>;
-}
