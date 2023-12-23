cbf9009877fcb2bae71dfbf2619a3c29c43fa77f
myjeong19@naver.com <myjeong19@naver.com>
2023-12-23 17:12:17 +0900

    Delete Task

--- todolist/src/components/main/Tasks.jsx
+++ todolist/src/components/main/Tasks.jsx
@@ -1,15 +1,23 @@
+import { supabase } from '../../supabaseClient';
 import classes from './css/Tasks.module.css';
 import { MdDelete } from 'react-icons/md';
 
 export const Tasks = ({ tasks }) => {
   const elementTasks = tasks.map(todo => {
+    const handleDeleteTasks = async () => {
+      const { data, error } = await supabase
+        .from('todos')
+        .delete()
+        .eq('id', todo.id);
+    };
+
     return (
       <li className={classes.card} key={todo.id}>
         <h3>{todo.title}</h3>
 
         <div>
           <button>{todo.status}</button>
-          <button>
+          <button onClick={handleDeleteTasks}>
             <MdDelete />
           </button>
         </div>
